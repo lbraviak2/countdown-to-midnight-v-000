26d8ad323ac768e34dc8981e9e0b07c8b23e8b21
Lindsay Braviak <lbraviak@gmail.com>
2018-10-08 02:10:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,9 @@
 
 def countdown (num)
 
-  while integer(num) > 0
+  while Int(num) > 0
     puts "#{num} SECOND(S)!"
-    integer(num) = integer(num) - 1
+    Int(num) = Int(num) - 1
   end
   puts "HAPPY NEW YEAR!"
 
