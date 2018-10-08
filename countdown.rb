497aabaab45f3a109cc1788f6f69613576c2c848
Lindsay Braviak <lbraviak@gmail.com>
2018-10-08 02:12:04 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,9 @@
 def countdown (number)
 
   while number > 0
-    puts "#{number} SECOND(S)!"
+    return "#{number} SECOND(S)!"
     number -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 
 end
